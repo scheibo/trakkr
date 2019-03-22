5c33504c53c4d1ddbb3b0721ad0843a798c85082
Kirk Scheibelhut <kjs@google.com>
2019-03-22 13:16:52 -0700

    Get something workable with new trakr API

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import * as colors from 'colors/safe';
 import {table} from 'table';
 import * as trakr from 'trakr';
 
+export {Timer, TRACER} from 'trakr';  // TODO
+
 type Formatted = string;
 type Raw = (number|string);
 type Data = Array<[Formatted[], Raw[]]>;
@@ -20,33 +22,20 @@ export class Formatter {
     this.fn = fn;
   }
 
-  display<T extends Tracker>(t: T): string {
-    return t.display(this);
-  }
-}
-
-export class Tracker extends trakr.Tracker {
-  display(fmt: Formatter = TABLE_FORMATTER): string {
-    // TODO
-    return '';
-  }
-}
-
-export class Timer extends trakr.Timer {
-  display(fmt: Formatter = TABLE_FORMATTER): string {
+  display(t: trakr.Timer): string {
     let out = '';
 
-    const stats = this.stats();
-    const total = this.duration;
+    const stats = t.stats();
+    const total = t.duration;
 
     if (total) {
       out += `\n${colors.bold(colors.underline('Time'))}` +
-          `(${Timer.format(total)})\n`;
+          ` (${format(total)})\n`;
     }
 
     if (stats.size) {
       const header = ['name', 'tot', 'cnt', 'p50', 'p90', 'p95', 'p99'];
-      if (fmt.full) header.push('min', 'max', 'avg', 'std');
+      if (this.full) header.push('min', 'max', 'avg', 'std');
       const maxes = (new Array(header.length)).fill(-Infinity);
 
       const data: Data = [];
@@ -61,26 +50,26 @@ export class Timer extends trakr.Timer {
             formatted.push(`${r}`);
             continue;
           }
-          let f = Timer.format(r as number);
-          if (total) f += ` (${Timer.percent(r, total)})`;
+          let f = format(r as number);
+          if (total) f += ` (${percent(r, total)})`;
           formatted.push(f);
         }
         multimax(maxes, formatted);
         data.push([formatted, raw]);
       }
 
-      if (fmt.sort) data.sort((a, b) => fmt.sort!(header, a[1], b[1]));
-      out += fmt.fn(header, data, maxes);
+      if (this.sort) data.sort((a, b) => this.sort!(header, a[1], b[1]));
+      out += this.fn(header, data, maxes);
     }
 
-    if (this.counters.size) {
+    if (t.counters.size) {
       if (!stats.size) out += '\n';
       out += `\n${colors.bold(colors.underline('Counters'))}\n`;
 
       const header = ['name', 'count'];
 
       const groups = new Map();
-      for (const [name, count] of this.counters.entries()) {
+      for (const [name, count] of t.counters.entries()) {
         const [prefix, suffix] = name.split(/:/, 2);
         let group = groups.get(prefix);
         if (!group) groups.set(prefix, (group = []));
@@ -88,22 +77,16 @@ export class Timer extends trakr.Timer {
       }
 
       for (const [group, data] of groups.entries()) {
-        if (fmt.sort) {
-          data.sort((a: Raw[], b: Raw[]) => fmt.sort!(header, a, b));
+        if (this.sort) {
+          data.sort((a: Raw[], b: Raw[]) => this.sort!(header, a, b));
         }
-        out += fmt.fn(header, data);
+        out += this.fn(header, data);
       }
     }
 
     return out;
   }
-
-  static percent(n: number, d: number): string {
-    return `${(n * 100 / d).toFixed(2)}%`;
-  }
 }
-
-export class Stats extends trakr.Stats {}
 
 export function TABLE(header: string[], data: Data, breaks?: number[]) {
   const formatted = header.length === 2 ? data : data.map(d => d[0]);
@@ -136,15 +119,33 @@ function sv(sep: string) {
   };
 }
 
+function format(ms: number): string {
+  if (ms < 0.001) return `${decimal(ms * 1000 * 1000)}ns`;
+  if (ms < 1) return `${decimal(ms * 1000)}\u03BCs`;
+  if (ms < 1000) return `${decimal(ms)}ms`;
+  return `${decimal(ms / 1000)}s`;
+}
+
+function decimal(n: number): string {
+  if (n < 1) return n.toFixed(3);
+  if (n < 10) return n.toFixed(2);
+  if (n < 100) return n.toFixed(1);
+  return n.toFixed();
+}
+
 function readable(s: string): string {
   if (s === 'cnt') return 'count';
   if (s === 'tot') return 'total';
   return s;
 }
 
+function percent(n: number, d: number): string {
+  return `${(n * 100 / d).toFixed(2)}%`;
+}
+
 function multimax(maxes: number[], data: string[]) {
   for (let i = 0; i < data.length; i++) {
-    const len = Stats.max(data[i].split(' ').map(d => d.length));
+    const len = trakr.Stats.max(data[i].split(' ').map(d => d.length));
     if (len > maxes[i]) maxes[i] = len;
   }
 }
